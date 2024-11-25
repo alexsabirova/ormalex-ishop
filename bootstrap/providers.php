356a257cba56df8aed0f2bc180cd2f57e7d82bc0
alexsabirova <alexsabirova@gmail.com>
2024-11-25 14:05:16 +0700

    refactor(provider): fix provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\DomainServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
-    App\Providers\ViewServiceProvider::class,
     App\Providers\TestingServiceProvider::class,
+    App\Providers\ViewServiceProvider::class,
 ];
