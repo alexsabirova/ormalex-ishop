ca8757a7970cc752c2b8c3444cb0032427f41aa1
alexsabirova <alexsabirova@gmail.com>
2024-11-27 11:27:36 +0700

    refactor(action): fix NewUserAction with DTO

--- src/Domain/Auth/Actions/RegisterNewUserAction.php
+++ src/Domain/Auth/Actions/RegisterNewUserAction.php
@@ -5,18 +5,19 @@ declare(strict_types=1);
 namespace Domain\Auth\Actions;
 
 use Domain\Auth\Contracts\RegisterNewUserContract;
+use Domain\Auth\DTOs\NewUserDTO;
 use Domain\Auth\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Hash;
 
 final class RegisterNewUserAction implements RegisterNewUserContract
 {
-    public function __invoke(string $name, string $email, string $password): void
+    public function __invoke(NewUserDTO $data): void
     {
         $user = User::query()->create([
-            'name' => $name,
-            'email' => $email,
-            'password' => Hash::make($password)
+            'name' => $data->name,
+            'email' => $data->email,
+            'password' => Hash::make($data->password)
         ]);
 
         event(new Registered($user));
