3b3dba0e952fee65dbc1d588903ccd97c273ce7d
alexsabirova <alexsabirova@gmail.com>
2024-12-03 14:59:30 +0700

    refactor(property): add collection and factory

---
+++ src/Domain/Product/Collections/PropertyCollection.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Domain\Product\Collections;
+
+use Illuminate\Database\Eloquent\Collection;
+
+final class PropertyCollection extends Collection
+{
+    public function keyValues()
+    {
+        return $this->mapWithKeys(fn($property) => [
+            $property->title => $property->pivot->value
+        ]);
+    }
+}
